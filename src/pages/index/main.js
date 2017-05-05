3426edf9eb0b7750be850854e5dcb2ed68f5368c
bennyzheng <bennyzheng@lexinfintech.com>
2017-05-05 20:34:40 +0800

    提供了babel-polyfill功能以支持更多浏览器(比如IE)

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,13 +1,13 @@
+// 若不需要考虑兼容性可以不包含babel-polyfill，仅需在入口文件包含
 import "babel-polyfill";
+import "./main.scss";
+import "utils/interceptors";
 import Vue from "vue";
 import Vuex from "vuex";
 import VueRouter from "vue-router";
-import css from "./main.scss";
-import axios from "axios";
 import store from "./store";
 import routes from "./routes";
 import AppNav from "./nav.vue";
-import interceptors from "utils/interceptors";
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
